f82db1190de07c4b556edd7e8c22f933b33c68e1
zohar <z.kapach@oqullo.com>
2022-06-26 16:30:48 -0400

    implemented step 1

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 #load and split iris data into the features and labeled sets
 data = load_iris()
 features = data["data"]
 labels = data["target"]
+
+#STEP 1: Split the data so 80% goes into training and 20% goes into testing
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.2 )
 
 #initialize and fit the model to the data
 model = DecisionTreeClassifier()
