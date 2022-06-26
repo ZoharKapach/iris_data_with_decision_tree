892bef7cd204fa4e7b78e365a65a294f28dc7848
zohar <z.kapach@oqullo.com>
2022-06-26 16:52:32 -0400

    implemented step 3

--- model.py
+++ model.py
@@ -18,7 +18,28 @@ model.fit(features_train, labels_train)
 #test the mdoel
 predictions = model.predict(features_test)
 
-#Step2: measure the accuracy in two different ways
+#STEP 2: measure the accuracy in two different ways
 accuracy = accuracy_score(labels_test, predictions)
 accuracy2 = model.score(features_test, labels_test)
-print("The mean accuracy of the model is {}.".format(accuracy))
+print("The mean accuracy of the model is {}.".format(accuracy2))
+
+
+#STEP 3: cross validate and check which max_depth is the best for this model. This will likely help avoid overfitting.
+max_depths = [1, 5, 10]
+best_depth = {"depth": [],
+              "accuracy": -1}
+
+for max_depth in max_depths:
+    model = DecisionTreeClassifier(max_depth=max_depth)
+    model.fit(features_train, labels_train)
+    accuracy2 = model.score(features_test, labels_test)
+    if accuracy2 == best_depth["accuracy"]:
+        best_depth["depth"].append(max_depth)
+    if accuracy2 > best_depth["accuracy"]:
+        best_depth["depth"] = [max_depth]
+        best_depth["accuracy"] = accuracy2
+        
+if len(best_depth["depth"]) > 1:
+    print("The best max depths for the model are {}, each with an accuracy of {}".format(best_depth["depth"], best_depth["accuracy"]))
+else:
+    print("The best max depth for the model is {}, with an accuracy of {}".format(best_depth["depth"][0], best_depth["accuracy"]))
